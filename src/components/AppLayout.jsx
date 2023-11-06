467596a310411c64b685d73485eb1e66f2baddc2
Zawalid <walid.zakan@gmail.com>
2023-11-06 09:27:50 +0100

    Style the sonner toast

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -8,7 +8,16 @@ import { Toaster } from 'sonner';
 export default function AppLayout() {
   return (
     <>
-      <Toaster position='top-right' />
+      <Toaster
+        position='top-right'
+        closeButton={true}
+        loadingIcon={
+          <i className='fa-solid fa-spinner animate-spin text-lg text-text-secondary'></i>
+        }
+        toastOptions={{
+          className: 'sonner-toast',
+        }}
+      />
       <div className='flex h-full gap-2 bg-background-primary p-5'>
         <SearchProvider>
           <Menu />
